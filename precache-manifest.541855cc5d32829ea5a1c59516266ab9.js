007a6a4eae5ea50aa79d4256870492d90d1c3447
JuneHee Lee <alfzm2020@gmail.com>
2020-07-09 14:45:05 +0900

    Updates

---
+++ precache-manifest.541855cc5d32829ea5a1c59516266ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac7443969713bf837de8d846b998c90",
+    "url": "/moive_app_2020/index.html"
+  },
+  {
+    "revision": "8cad100c60de2fd0d1b4",
+    "url": "/moive_app_2020/static/css/main.de27eb2c.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/moive_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moive_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cad100c60de2fd0d1b4",
+    "url": "/moive_app_2020/static/js/main.0c9babe9.chunk.js"
+  },
+  {
+    "revision": "4efb30d0eb0a0120c80f",
+    "url": "/moive_app_2020/static/js/runtime-main.25d2d607.js"
+  }
+]);
